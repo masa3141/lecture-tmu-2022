6513612f9044f39d961aef5b9db9a86435236fa1
masa3141 <masahiro@kazama.tv>
2022-09-14 01:00:18 +0900

    modify space

--- app/main.py
+++ app/main.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
     # Use this for debugging purposes only
     import uvicorn
 
-    uvicorn.run(app,host="0.0.0.0", port=8001, log_level="debug")
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
